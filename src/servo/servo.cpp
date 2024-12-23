3c430295a3fdbf7c662e6d4decd186e97d4b4a66
Roland Branten <roland.branten@gmail.com>
2024-12-23 09:50:00 +0100

    feat: detach servo after use

--- src/servo/servo.cpp
+++ src/servo/servo.cpp
@@ -1,13 +1,16 @@
-// servo.cpp
-
 #include "servo/servo.h"
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include "buzzer/buzzer.h"
 #include "util/global.h"
 
-Servo myservo;		// Declare the servo object
-static int pos = 0; // Variable to store the servo position
+Servo myservo; // Declare the servo object
+
+static const int open = 35;	  // Constant to represent the open position
+static const int locked = 62; // Constant to represent the locked position
+static const int detachTime = 500;
+
+static int pos = open; // Variable to store the servo position
 
 // Define pins
 static const int servoPin = 21; // Pin D21 for the servo
@@ -18,29 +21,48 @@ void servoSetup()
 	myservo.attach(servoPin);
 
 	// Initialize the servo position to 0 degrees
-	myservo.write(0);
+	myservo.write(open);
+
+	unsigned long startTime = millis(); // Record the current time
+	while (millis() - startTime < detachTime)
+	{
+		// Wait for 1000ms without blocking other operations
+	}
+
+	myservo.detach(); // Turn off the servo to save power
 }
 
 void toggleLock()
 {
 	shouldBuzz = true;
 
+	myservo.attach(servoPin); // Reattach servo before moving
+
 	if (lockClosed)
 	{
-		// Move servo from 0 to 180 degrees
-		for (pos = 0; pos <= 180; pos += 5)
+		// Move servo from open to locked position
+		for (pos = open; pos <= locked; pos += 5)
 		{
 			myservo.write(pos);
 		}
 	}
 	else
 	{
-		// Move servo from 180 to 0 degrees
-		for (pos = 180; pos >= 0; pos -= 5)
+		// Move servo from locked to open position
+		for (pos = locked; pos >= open; pos -= 5)
 		{
 			myservo.write(pos);
 		}
 	}
+
+	// Record the start time for the 2000ms delay
+	unsigned long startTime = millis();
+	while (millis() - startTime < detachTime)
+	{
+		// Wait for 1000ms without blocking other operations
+	}
+
+	myservo.detach(); // Turn off the servo to save power
 
 	// Toggle the direction for the next button press
 	lockClosed = !lockClosed;
